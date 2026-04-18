c1e9cd5ade0e2536850eaa6e9a73bc3e7f3a418f
marcos jimenez <marcosjv1644@gmail.com>
2026-04-18 20:21:45 +0200

    SQLite added to the project

--- src/app/extras/page.tsx
+++ src/app/extras/page.tsx
@@ -2,19 +2,22 @@
 
 import Navbar from "../components/Navbar";
 import QuizPMD from "../components/QuizPMD";
+import { useLanguage } from "@/context/LanguageContext";
+
 
 export default function ExtrasPage() {
+    const { lang } = useLanguage();
   return (
     <>
       <Navbar />
 
       <section className="w-full min-h-screen py-20 px-6 flex flex-col items-center">
         <h1 className="text-4xl md:text-5xl font-bold mb-6">
-          Test de Personalidad – Pokémon Mundo Misterioso
+          {lang === "es" ? "Personalidad Pokémon" : "Pokémon Personality"}
         </h1>
 
         <p className="text-lg opacity-80 max-w-2xl text-center mb-10">
-          Responde a las preguntas y descubre qué Pokémon serías en Pokémon Mundo Misterioso.
+          {lang === "es" ? "Responde a las preguntas y descubre qué Pokémon eres espiritualmente." : "Answer the questions and discover what pokémon are you spiritually"}
         </p>
 
         <QuizPMD />
